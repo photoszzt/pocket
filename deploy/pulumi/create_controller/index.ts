5937315966687b076fbaa26bd1823f591366e798
Zhiting Zhu <zhitingz@cs.utexas.edu>
2020-08-21 16:24:09 +0000

    generate env.sh file
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- deploy/pulumi/create_controller/index.ts
+++ deploy/pulumi/create_controller/index.ts
@@ -9,8 +9,17 @@ const keyName = config.require('keyName')
 
 const stack = pulumi.getStack()
 const vpc = new pulumi.StackReference(`${org}/create_vpc/${stack}`);
-const privateSubnetAvailabilityZone = vpc.getOutput("subnetPrivateAz");
-const privateSubnetId = vpc.getOutput("privateSubnetId")
+
+const publicSubnetAz = vpc.getOutput("subnetPublicAz");
+const publicSubnetId = vpc.getOutput("publicSubnetId");
+const publicSubnetCidr = vpc.getOutput("subnetPublicCidr");
+
+const privateSubnetAz = vpc.getOutput("subnetPrivateAz");
+const privateSubnetId = vpc.getOutput("privateSubnetId");
+const privateSubnetCidr = vpc.getOutput("subnetPrivateCidr");
+
+const pocketVpcId = vpc.getOutput("POCKET_VPC_ID");
+
 const pocketControllerPrivateIP = "10.1.47.178";
 const bastionSgId = vpc.getOutput("bastionSgId")
 
@@ -28,9 +37,24 @@ sudo yum install -y python3
 cd ~
 python3 -m venv k8s
 source activate k8s/bin/activate
-pip install -U pip setuptools
-pip install kubernetes
-pip install awscli`
+python3 -m pip install -U pip setuptools
+python3 -m pip install kubernetes
+python3 -m pip install awscli
+
+git clone https://github.com/photoszzt/pocket.git
+
+echo "export NAME=pocketcluster.k8s.local" > ~/env.sh
+echo "export KOPS_STATE_STORE=s3://zzt-videos" >> ~/env.sh
+echo "export POCKET_VPC_ID=${pocketVpcId}" >> ~/env.sh
+
+echo "export POCKET_VPC_PRIVATE_SUBNET_ID=${privateSubnetId}" >> ~/env.sh
+echo "export POCKET_VPC_PUBLIC_SUBNET_ID=${publicSubnetId}" >> ~/env.sh
+
+echo "export POCKET_VPC_NETWORK_CIDR=${pocketVpcId}" >> ~/env.sh
+echo "export POCKET_VPC_PRIVATE_NETWORK_CIDR=${privateSubnetCidr}" >> ~/env.sh
+echo "export POCKET_VPC_PUBLIC_NETWORK_CIDR=${publicSubnetCidr}" >> ~/env.sh
+
+echo "export POCKET_AWS_ZONE=${privateSubnetAz}" >> ~/env.sh`
 
 const pocket_controller = new aws.ec2.Instance("pocket-controller", {
     ami: "ami-02354e95b39ca8dec",
@@ -38,7 +62,7 @@ const pocket_controller = new aws.ec2.Instance("pocket-controller", {
     tags: {
         Name: "PocketController",
     },
-    availabilityZone: privateSubnetAvailabilityZone,
+    availabilityZone: privateSubnetAz,
     privateIp: pocketControllerPrivateIP,
     subnetId: privateSubnetId,
     associatePublicIpAddress: true,
