d3565f6532332c036748b39c93f3ab3b6c978890
zhitingz <zhitingz@cs.utexas.edu>
2020-08-21 22:21:24 +0000

    update cluster setup
    Signed-off-by: zhiting zhu <zhitingz@cs.utexas.edu>

--- deploy/setup_cluster.sh
+++ deploy/setup_cluster.sh
@@ -1,8 +1,21 @@
 #/bin/bash
 
-kops create -f $NAME.yaml
-kops create secret --name pocketcluster.k8s.local sshpublickey admin -i ~/.ssh/id_rsa.pub
-kops update cluster ${NAME} --yes
+set -euo pipefail
+if [ $# -ne 1 ]; then
+    echo "please provide config json file"
+    exit 1
+fi
+
+CONFIG=$(cat $1)
+CLUSTER_NAME="$(echo ${CONFIG} | jq -r .NAME)"
+
+kops toolbox template --name ${CLUSTER_NAME} --values <( echo ${CONFIG}) --template pocketcluster.template.yaml --format-yaml > pocketcluster.k8s.local.yaml
+
+kops replace -f pocketcluster.k8s.local.yaml --name ${CLUSTER_NAME} --force
+
+kops create secret --name ${CLUSTER_NAME} sshpublickey admin -i ~/.ssh/id_rsa.pub
+
+kops update cluster --name ${CLUSTER_NAME} --yes
 
 #  Then wait. 
 #  When cluster running (check with `kops validate cluster`), run the following commands:
