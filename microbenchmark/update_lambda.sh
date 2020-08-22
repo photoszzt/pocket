64b5f856d11fdc0a0b7e39173166e1dfeb873de2
Zhiting Zhu <zhitingz@cs.utexas.edu>
2020-08-22 17:10:45 +0000

    update lib
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- microbenchmark/update_lambda.sh
+++ microbenchmark/update_lambda.sh
@@ -2,11 +2,9 @@
 
 rm deploy.zip
 
-zip -r deploy.zip latency.py pocket.py libc.so.6 libstdc++.so.6 libpocket.so libcppcrail.so libboost_python-py35.so.1.58.0 
+zip -r deploy.zip latency.py pocket.py libc.so.6 libstdc++.so.6 libpocket.so libcppcrail.so \
+    libboost_python-py36.so.*
 
 aws lambda update-function-code \
     --function-name pocket_latency_test \
     --zip-file fileb://deploy.zip \
-
-
-
