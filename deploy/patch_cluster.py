30e29afd0ae515412668b6407309eb95ceb78c84
Zhiting Zhu <zhitingz@cs.utexas.edu>
2020-08-22 17:47:37 +0000

    futurize patch cluster
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- deploy/patch_cluster.py
+++ deploy/patch_cluster.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import subprocess as sp
 import shlex
 from subprocess import Popen, PIPE
@@ -27,22 +28,22 @@ def prep_i3instances(private_subnetid, node_groupid):
     pattern = r'"([A-Za-z0-9_\./\\-]*)"'
     p = re.compile(pattern)
     i3_instance_ids = p.findall(out) 
-    print "i3 instance ids findall: ", i3_instance_ids 
+    print("i3 instance ids findall: ", i3_instance_ids) 
     
     for i3_instance_id in i3_instance_ids:
         # create elastic network interface 
         create_eni_command = "aws ec2 create-network-interface --subnet-id " + private_subnetid + " --description \"eni for i3\" --groups " \
                                + node_groupid  
-        print create_eni_command
+        print(create_eni_command)
         exitcode, out, err = get_exitcode_stdout_stderr(create_eni_command)
-        print out
+        print(out)
         pattern = r'"(eni-[A-Za-z0-9]+)"'
         i3_eni = re.search(pattern, out).group().strip('\"')
-        print "i3 ENI id is: " + i3_eni
+        print("i3 ENI id is: " + i3_eni)
     
         # attach eni to i3 instance
         attach_eni_command = "aws ec2 attach-network-interface --network-interface-id " + i3_eni + " --instance-id " + i3_instance_id + " --device-index 1"
-        print attach_eni_command
+        print(attach_eni_command)
         sp.call(attach_eni_command, shell=True)
    
 
@@ -54,22 +55,22 @@ def prep_h1instances(private_subnetid, node_groupid):
     pattern = r'"([A-Za-z0-9_\./\\-]*)"'
     p = re.compile(pattern)
     h1_instance_ids = p.findall(out) 
-    print "h1 instance ids findall: ", h1_instance_ids 
+    print("h1 instance ids findall: ", h1_instance_ids) 
     
     for h1_instance_id in h1_instance_ids:
         # create elastic network interface 
         create_eni_command = "aws ec2 create-network-interface --subnet-id " + private_subnetid + " --description \"eni for h1\" --groups " \
                                + node_groupid  
-        print create_eni_command
+        print(create_eni_command)
         exitcode, out, err = get_exitcode_stdout_stderr(create_eni_command)
-        print out
+        print(out)
         pattern = r'"(eni-[A-Za-z0-9]+)"'
         h1_eni = re.search(pattern, out).group().strip('\"')
-        print "h1 ENI id is: " + h1_eni
+        print("h1 ENI id is: " + h1_eni)
     
         # attach eni to h1 instance
         attach_eni_command = "aws ec2 attach-network-interface --network-interface-id " + h1_eni + " --instance-id " + h1_instance_id + " --device-index 1"
-        print attach_eni_command
+        print(attach_eni_command)
         sp.call(attach_eni_command, shell=True)
 
 def prep_i2instances(private_subnetid, node_groupid):
@@ -80,22 +81,22 @@ def prep_i2instances(private_subnetid, node_groupid):
     pattern = r'"([A-Za-z0-9_\./\\-]*)"'
     p = re.compile(pattern)
     i2_instance_ids = p.findall(out) 
-    print "i2 instance ids findall: ", i2_instance_ids 
+    print("i2 instance ids findall: ", i2_instance_ids) 
     
     for i2_instance_id in i2_instance_ids:
         # create elastic network interface 
         create_eni_command = "aws ec2 create-network-interface --subnet-id " + private_subnetid + " --description \"eni for i2\" --groups " \
                                + node_groupid  
-        print create_eni_command
+        print(create_eni_command)
         exitcode, out, err = get_exitcode_stdout_stderr(create_eni_command)
-        print out
+        print(out)
         pattern = r'"(eni-[A-Za-z0-9]+)"'
         i2_eni = re.search(pattern, out).group().strip('\"')
-        print "i2 ENI id is: " + i2_eni
+        print("i2 ENI id is: " + i2_eni)
     
         # attach eni to i2 instance
         attach_eni_command = "aws ec2 attach-network-interface --network-interface-id " + i2_eni + " --instance-id " + i2_instance_id + " --device-index 1"
-        print attach_eni_command
+        print(attach_eni_command)
         sp.call(attach_eni_command, shell=True)
 
 def add_namenode_eni():
@@ -114,7 +115,7 @@ def add_namenode_eni():
     # create elastic network interface with predefined IP
     create_eni_command = "aws ec2 create-network-interface --subnet-id " + private_subnetid + " --description \"eni for namenode\" --groups " \
                            + node_groupid + " --private-ip-address " + NAMENODE_IP 
-    print create_eni_command
+    print(create_eni_command)
     sp.call(create_eni_command, shell=True)
     
     # get namenode eni id
@@ -122,7 +123,7 @@ def add_namenode_eni():
     exitcode, out, err = get_exitcode_stdout_stderr(cmd)
     pattern = r'"([A-Za-z0-9_\./\\-]*)"'
     namenode_eni = re.search(pattern, out).group().strip('\"')
-    print "Namenode ENI id is: " + namenode_eni
+    print("Namenode ENI id is: " + namenode_eni)
 
 
     # get instance id for namendoe
@@ -134,7 +135,7 @@ def add_namenode_eni():
  
     # attach eni to namenode
     attach_eni_command = "aws ec2 attach-network-interface --network-interface-id " + namenode_eni + " --instance-id " + namenode_instance_id + " --device-index 1"
-    print attach_eni_command
+    print(attach_eni_command)
     sp.call(attach_eni_command, shell=True)
 
     return private_subnetid, node_groupid
@@ -155,7 +156,7 @@ def add_namenode_eni():
     # create elastic network interface with predefined IP
     create_eni_command = "aws ec2 create-network-interface --subnet-id " + private_subnetid + " --description \"eni for namenode\" --groups " \
                            + node_groupid + " --private-ip-address " + NAMENODE_IP 
-    print create_eni_command
+    print(create_eni_command)
     sp.call(create_eni_command, shell=True)
     
     # get namenode eni id
@@ -163,7 +164,7 @@ def add_namenode_eni():
     exitcode, out, err = get_exitcode_stdout_stderr(cmd)
     pattern = r'"([A-Za-z0-9_\./\\-]*)"'
     namenode_eni = re.search(pattern, out).group().strip('\"')
-    print "Namenode ENI id is: " + namenode_eni
+    print("Namenode ENI id is: " + namenode_eni)
 
 
     # get instance id for namendoe
@@ -175,7 +176,7 @@ def add_namenode_eni():
  
     # attach eni to namenode
     attach_eni_command = "aws ec2 attach-network-interface --network-interface-id " + namenode_eni + " --instance-id " + namenode_instance_id + " --device-index 1"
-    print attach_eni_command
+    print(attach_eni_command)
     sp.call(attach_eni_command, shell=True)
 
     return private_subnetid, node_groupid
@@ -197,7 +198,7 @@ def add_lambda_security_group_ingress_rule():
 
     # add ingress rule for node group to accept traffic from pocket-kubernetes-lax group (this is group lamdbas will be in)
     modify_security_group_command = 'aws ec2 authorize-security-group-ingress --group-id ' + node_groupid + ' --protocol all --source-group ' + pocket_lax_groupid
-    print modify_security_group_command
+    print(modify_security_group_command)
     sp.call(modify_security_group_command, shell=True)
 
 
