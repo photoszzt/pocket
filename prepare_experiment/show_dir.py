e13ba45b89acd1f0849e742787df58e0f02c4c44
Zhiting Zhu <zhitingz@cs.utexas.edu>
2020-09-02 22:11:23 +0000

    show dir more flexible
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- prepare_experiment/show_dir.py
+++ prepare_experiment/show_dir.py
@@ -1,7 +1,17 @@
+import argparse
 import pocket_api
 
-namenode_ip = "10.1.0.10"
-p = pocket_api.connect(namenode_ip, 9070)
-listed = pocket_api.list_dir(p, "/", "")
-files = [f for f in listed]
-print(files)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", default="/")
+    args = parser.parse_args()
+    namenode_ip = "10.1.0.10"
+    p = pocket_api.connect(namenode_ip, 9070)
+    listed = pocket_api.list_dir(p, args.dir, "")
+    files = [f for f in listed]
+    print(files)
+
+
+if __name__ == '__main__':
+    main()
