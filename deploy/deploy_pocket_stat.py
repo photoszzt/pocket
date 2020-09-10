63abf486e3e2d4341fe9bdc247e062a5f78f901b
Zhiting Zhu <zhitingz@cs.utexas.edu>
2020-09-10 20:17:29 +0000

    fix some bugs
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

---
+++ deploy/deploy_pocket_stat.py
@@ -0,0 +1,23 @@
+from os import path
+import time
+
+import yaml
+
+from kubernetes import client, config
+
+
+def main():
+    config.load_kube_config()
+
+    with open(path.join(path.dirname(__file__), "pocket-stat-daemonset.yaml")) as f:
+        # use safe load to disable warnings
+        # https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation
+        dep = yaml.safe_load(f)
+        k8s_beta = client.ExtensionsV1beta1Api()
+        resp = k8s_beta.create_namespaced_daemon_set(
+            body=dep, namespace="default")
+        print("Deployment created. status='%s'" % str(resp.status))
+
+
+if __name__ == '__main__':
+    main()
