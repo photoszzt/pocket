7d190e649c864a473814ce796f2a2383358a416a
Zhiting Zhu <zhitingz@cs.utexas.edu>
2020-12-07 18:10:57 +0000

    refactor scripts
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- deploy/pulumi/gen_config.sh
+++ deploy/pulumi/gen_config.sh
@@ -2,18 +2,6 @@
 set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-if [ $# -ne 1 ]; then
-  echo "need to enter key file name used to access controller and bastion"
-  exit 1
-fi
-
-keyFile=$1
-
-cd $SCRIPT_DIR/create_controller
-CONTROLLER_OUT=$(pulumi stack output -j)
-CONTROLLER_IP=$(echo $CONTROLLER_OUT | jq -r .pocketControllerPrivateIP)
-CONTROLLER_DNS=$(echo $CONTROLLER_OUT | jq -r .controllerPublicDNS)
-cd -
 
 cd $SCRIPT_DIR/create_vpc
 VPC_OUT=$(pulumi stack output -j)
@@ -45,12 +33,3 @@ echo "  \"POCKET_VPC_NETWORK_CIDR\": \"${pocketVPCNetworkCidr}\"," >> $SCRIPT_DI
 echo "  \"POCKET_VPC_PUBLIC_NETWORK_CIDR\": \"${publicSubnetCidr}\"," >> $SCRIPT_DIR/pocket-k8s-config.json
 echo "  \"POCKET_AWS_ZONE\": \"${publicSubnetAz}\"" >> $SCRIPT_DIR/pocket-k8s-config.json
 echo "}" >> $SCRIPT_DIR/pocket-k8s-config.json
-
-scp -i $keyFile $SCRIPT_DIR/env.sh ubuntu@$CONTROLLER_DNS:~
-scp -i $keyFile  $SCRIPT_DIR/pocket-k8s-config.json \
-  ubuntu@$CONTROLLER_DNS:~
-# also setup the credential
-ssh -i $keyFile ubuntu@$CONTROLLER_DNS "mkdir -p ~/.aws && source \
-  /home/ubuntu/k8s/bin/activate && ~/k8s/bin/aws configure set region us-east-1"
-scp -i $keyFile $HOME/.aws/credentials \
-  ubuntu@$CONTROLLER_DNS:~/.aws/
